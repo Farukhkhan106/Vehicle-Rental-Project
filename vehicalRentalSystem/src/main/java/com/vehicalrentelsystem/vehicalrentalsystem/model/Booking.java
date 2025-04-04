9ec2be696bdd85b92d2efcef583feeb7ec62174b
blood <farukhmansuri11122@gmail.com>
2025-04-04 17:21:14 +0530

    Added backend files and updated frontend

---
+++ vehicalRentalSystem/src/main/java/com/vehicalrentelsystem/vehicalrentalsystem/model/Booking.java
@@ -0,0 +1,32 @@
+//package com.vehicalrentelsystem.vehicalrentalsystem.model;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//import java.time.LocalDate;
+//
+//@Entity
+//@Table(name = "bookings")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Booking {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "vehicle_id", nullable = false)
+//    private Vehicle vehicle;
+//
+//    private LocalDate startDate;
+//    private LocalDate endDate;
+//
+//    @Enumerated(EnumType.STRING)
+//    private BookingStatus status;
+//}
